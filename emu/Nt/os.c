fa871c313191a5a5d28d5cbd18921daaea4d212a
Charles.Forsyth <devnull@localhost>
2009-05-26 14:42:13 +0000

    20090526-1541

--- emu/Nt/os.c
+++ emu/Nt/os.c
@@ -495,7 +495,15 @@ close(int fd)
 int
 read(int fd, void *buf, uint n)
 {
-	if(!ReadFile(ntfd2h(fd), buf, n, &n, NULL))
+	HANDLE h;
+
+	if(fd == 0)
+		h = kbdh;
+	else
+		h = ntfd2h(fd);
+	if(h == INVALID_HANDLE_VALUE)
+		return -1;
+	if(!ReadFile(h, buf, n, &n, NULL))
 		return -1;
 	return n;
 }
--- include/version.h
+++ include/version.h
@@ -1,1 +1,1 @@
-#define VERSION	"Fourth Edition (20090524)"
+#define VERSION	"Fourth Edition (20090526)"
