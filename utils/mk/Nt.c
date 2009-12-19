bdd7b97835b1dfefcf7ed63fac8f10cc09e66cb4
forsyth <forsyth@vitanuova.com>
2009-12-19 13:58:14 +0000

    20091219-1358

--- utils/mk/Nt.c
+++ utils/mk/Nt.c
@@ -11,8 +11,8 @@ enum {
 	Nchild	= 100,
 };
 
-char *rootdir =		"c:\\inferno";
-char *shell =		"Nt\\386\\bin\\rcsh.exe";	/* Path relative to root */
+char *rootdir =		ROOT;
+char *shell =		"Nt/386/bin/rcsh.exe";	/* Path relative to root */
 
 typedef struct Child	Child;
 
