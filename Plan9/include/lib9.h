8bdf904b6a23b93336ae2837772110bcdad234ce
Charles Forsyth <charles.forsyth@gmail.com>
2013-06-06 21:00:30 +0000

    change C library Runerror

--- Plan9/include/lib9.h
+++ Plan9/include/lib9.h
@@ -1,17 +1,11 @@
 #include <u.h>
 typedef usize size_t;
 
-#define	Runeerror xRuneerror
 #define	Rendez	xRendez
 #include <libc.h>
-#undef Runeerror
 #undef Rendez
 
 
-enum
-{
-	Runeerror	= 0x80,		/* decoding error in UTF */
-};
 /*
  *	Extensions for Inferno to basic libc.h
  */
