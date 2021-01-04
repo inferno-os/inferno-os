e273317811820976dda632b0893960646d6e1119
Valery Ushakov <uwe@stderr.spb.ru>
2021-01-04 03:00:19 +0300

    NetBSD/arm: segflush - use mprotect to add PROT_EXEC
    ARM has separate read and execute protection bits so after writing out
    JIT code we need to make it executable.

--- emu/NetBSD/segflush-arm.c
+++ emu/NetBSD/segflush-arm.c
@@ -1,4 +1,5 @@
 #include <sys/types.h>
+#include <sys/mman.h>
 #include <machine/sysarch.h>
 
 #include "dat.h"
@@ -8,6 +9,8 @@ int
 segflush(void *a, ulong n)
 {
 	struct arm_sync_icache_args args;
+
+	mprotect(a, (size_t)n, PROT_READ | PROT_WRITE | PROT_EXEC);
 
 	args.addr = (uintptr_t)a;
 	args.len = (size_t)n;
